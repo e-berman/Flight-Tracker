7425597a02fddaf3326928d85a7fd1060ebc6f6e
e-berman <ethanjamesberman@gmail.com>
2022-02-11 00:30:07 -0800

    Add basic framework for frontend + start controller/model

--- flight_tracker_react/src/App.js
+++ flight_tracker_react/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router,
+         Routes,
+         Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import ResultsPage from './pages/ResultsPage';
 
 function App() {
   return (
     <>
-    <Router>
-      <Route path='/' exact component={HomePage}/>
-      <Route path='/results' component={ResultsPage}/>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<HomePage/>}/>
+          <Route path='/results' element={<ResultsPage/>}/>
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ flight_tracker_react/src/pages/ResultsPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ResultsPage() {
+
+    return (
+        <>
+            <div className="header">
+                <h1>Results Page</h1>
+            </div>
+            <div>
+                <button>Send To Email</button>
+            </div>            
+        </>
+    );
+}
+
+export default ResultsPage;
