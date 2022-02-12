16a7c6680e11ddd94f36ada6a65f69dba0c437f9
e-berman <ethanjamesberman@gmail.com>
2022-02-12 00:51:42 -0800

    Update GUI and start price_scraper service

---
+++ price_scraper.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import time
+import datetime
+
+browser = webdriver.Chrome(executable_path='/chromedriver')
