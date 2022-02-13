61d7619f553c4e3c244011fbf7e6d99c0ce595b2
e-berman <ethanjamesberman@gmail.com>
2022-02-13 01:00:00 -0800

    build out app model + POST route for data to MongoDB

--- flight_tracker_rest/flight_tracker_controller.mjs
+++ flight_tracker_rest/flight_tracker_controller.mjs
@@ -1,3 +1,4 @@
+import * as flight from './flight_tracker_model.mjs';
 import express from 'express';
 
 const PORT = 3000;
@@ -11,9 +12,19 @@ app.use(express.json());
 // ROUTES GO HERE
 
 
-
-
-
+app.post('/', (req, res) => {
+    flight.createFlight(req.body.departingAirport, req.body.arrivingAirport, req.body.departingDate, req.body.arrivingDate)
+        // If no error, 201 status provided and object sent as response in JSON format
+        .then(flight_obj => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(201).json(flight_obj);
+        })
+        // In case of failed Promise, raise 404 status code
+        .catch(error => {
+            console.error(error);
+            res.status(400).json({ error: "Request Failed" });
+        });
+});
 
 
 //
--- flight_tracker_rest/flight_tracker_model.mjs
+++ flight_tracker_rest/flight_tracker_model.mjs
@@ -13,11 +13,48 @@ db.once('open', () => {
     console.log('Successfully connected to MongoDB using Mongoose.');
 });
 
-// Define schema for the flight object
-// const flightSchema = mongoose.Schema({
-//     name: { type: String, required: true },
-//     reps: { type: Number, required: true },
-//     weight: { type: Number, required: true},
-//     unit: { type: String, required: true},
-//     date: { type: String, required: true}
-// });
+//Define schema for the flight object
+const flightSchema = mongoose.Schema({
+    departingAirport: { type: String, required: true },
+    arrivingAirport: { type: String, required: true},
+    departingDate: { type: String, required: true},
+    arrivingDate: { type: String, required: true}
+});
+
+// Compile the model from the schema
+const Flight = mongoose.model('Flight', flightSchema);
+
+/**
+ * Creates an Flight object
+ * 
+ * @param {String} departingAirport
+ * @param {String} arrivingAirport
+ * @param {String} departingDate
+ * @param {String} arrivingDate
+ * @returns Promise - resolved to JSON after .save
+ */
+
+ const createFlight = async (departingAirport, arrivingAirport, departingDate, arrivingDate) => {
+    const flight = new Flight({ departingAirport: departingAirport,
+                                arrivingAirport: arrivingAirport,
+                                departingDate: departingDate,
+                                arrivingDate: arrivingDate });
+    return flight.save();
+}
+
+// /**
+//  * Reads Flight object(s)
+//  * 
+//  * No params
+//  * 
+//  * @returns all JSON objects in the collection
+//  */
+
+//  const readFlight = async (filter, projection, limit) => {
+//     const query = Flight.find(filter)
+//         .select(projection)
+//         .limit(limit);
+//     return query.exec();
+// }
+
+export { createFlight };
