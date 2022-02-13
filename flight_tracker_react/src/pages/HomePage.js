e8a65953b3e3ed52ee1379b0178dd11af6f6f5b5
e-berman <ethanjamesberman@gmail.com>
2022-02-13 01:04:24 -0800

    enable field data to be sent to database

--- flight_tracker_react/src/pages/HomePage.js
+++ flight_tracker_react/src/pages/HomePage.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
+
+    const [departingAirport, setDepartingAirport] = useState('');
+    const [arrivingAirport, setArrivingAirport] = useState('');
+    const [departingDate, setDepartingDate] = useState('');
+    const [arrivingDate, setArrivingDate] = useState('');
+
+    // with React v6, navigate replaces useHistory
+    const navigate = useNavigate();
+
+    // creates an flight and adds to database
+    const createFlight = async () => {
+        const newFlight = { departingAirport, arrivingAirport, departingDate, arrivingDate};
+        const response = await fetch('/', {
+            method: 'POST', 
+            body: JSON.stringify(newFlight),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        // error handling with response status check
+        if (response.status === 201) {
+            alert('Successfully added the flight');
+        } else {
+            alert(`Failed to add the flight. Status code = ${response.status}`);
+        }
+        navigate('/results');
+    }
+
 
     return (
         <>
@@ -9,23 +39,54 @@ function HomePage() {
                 <h1>Flight Tracker App</h1>
             </div>
             <div>
-                <input type='text' placeholder='Enter Airport Name..'/>
+                <select>
+                    <option>One Way</option>
+                    <option>Round Trip</option>
+                </select>
+            </div>
+            <div>
+                <label>
+                    Departing Airport:
+                    <input 
+                        type='text'
+                        placeholder='Enter Airport Abbreviation..'
+                        value={departingAirport}
+                        onChange={e => setDepartingAirport(e.target.value)}/>
+                </label>
                 <AiOutlineQuestionCircle />
             </div>
             <div>
                 <label>
+                    Arriving Airport:
+                    <input
+                        type='text'
+                        placeholder='Enter Airport Abbreviation..'
+                        value={arrivingAirport}
+                        onChange={e => setArrivingAirport(e.target.value)}/>
+                </label>
+            </div>
+            <div>
+                <label>
                     Departure Date: 
-                    <input type='date'/>
+                    <input
+                        type='date'
+                        placeholder='Enter Airport Abbreviation..'
+                        value={departingDate}
+                        onChange={e => setDepartingDate(e.target.value)}/>
                 </label>
             </div>
             <div>
                 <label>
                     Return Date: 
-                    <input type='date'/>
+                    <input
+                        type='date'
+                        placeholder='Enter Airport Abbreviation..'
+                        value={arrivingDate}
+                        onChange={e => setArrivingDate(e.target.value)}/>
                 </label>
             </div>
             <div>
-                <button>Generate</button>
+                <button onClick={createFlight}>Generate</button>
             </div>
 
             
