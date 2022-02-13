880f33a51a197658b609188307b10a99bb913466
e-berman <ethanjamesberman@gmail.com>
2022-02-13 01:04:58 -0800

    add homepage link

--- flight_tracker_react/src/pages/ResultsPage.js
+++ flight_tracker_react/src/pages/ResultsPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function ResultsPage() {
 
@@ -9,6 +10,9 @@ function ResultsPage() {
             </div>
             <div>
                 <button>Send To Email</button>
+            </div>
+            <div>
+                <Link to='/'>Search Again</Link>
             </div>            
         </>
     );
