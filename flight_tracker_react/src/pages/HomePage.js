f70b4f90eb108a561f22a8e0a43cb037935c9351
e-berman <ethanjamesberman@gmail.com>
2022-02-16 19:48:05 -0800

    Add bootstrap to app

--- flight_tracker_react/src/pages/HomePage.js
+++ flight_tracker_react/src/pages/HomePage.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { MdFlight } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
+// import 'boostrap/dist/css/boostrap.min.css';
+import { Container, Row, Col, Button, Form, Navbar } from 'react-bootstrap';
 
 function HomePage() {
 
@@ -34,63 +37,88 @@ function HomePage() {
 
 
     return (
-        <>
-            <div className="header">
-                <h1>Flight Tracker App</h1>
-            </div>
-            <div>
-                <select>
-                    <option>One Way</option>
-                    <option>Round Trip</option>
-                </select>
-            </div>
-            <div>
-                <label>
-                    Departing Airport:
-                    <input 
-                        type='text'
-                        placeholder='Enter Airport Abbreviation..'
-                        value={departingAirport}
-                        onChange={e => setDepartingAirport(e.target.value)}/>
-                </label>
-                <AiOutlineQuestionCircle />
-            </div>
-            <div>
-                <label>
-                    Arriving Airport:
-                    <input
-                        type='text'
-                        placeholder='Enter Airport Abbreviation..'
-                        value={arrivingAirport}
-                        onChange={e => setArrivingAirport(e.target.value)}/>
-                </label>
-            </div>
-            <div>
-                <label>
-                    Departure Date: 
-                    <input
-                        type='date'
-                        placeholder='Enter Airport Abbreviation..'
-                        value={departingDate}
-                        onChange={e => setDepartingDate(e.target.value)}/>
-                </label>
-            </div>
-            <div>
-                <label>
-                    Return Date: 
-                    <input
-                        type='date'
-                        placeholder='Enter Airport Abbreviation..'
-                        value={arrivingDate}
-                        onChange={e => setArrivingDate(e.target.value)}/>
-                </label>
-            </div>
-            <div>
-                <button onClick={createFlight}>Generate</button>
-            </div>
 
-            
-        </>
+        <div className="homepage">
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <MdFlight />
+                    Cheap Flights
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+            <Container>
+            <Form className="mt-5">
+                <Row>
+                    <Col>
+                        <Button size="xs" variant="outline-dark">Need Help?</Button>
+                    </Col>
+                </Row>
+                <Row className="mt-3">
+                    <Col>
+                    <Form.Group controlId="flightType">
+                        <Form.Label>Flight Type: </Form.Label>
+                        <Form.Select>
+                            <option>One Way</option>
+                            <option>Round Trip</option>
+                        </Form.Select>
+                    </Form.Group>
+                    </Col>
+                    <Col>
+                    <Form.Group controlId="departingAirport">
+                        <Form.Label>Departing Airport: </Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder='Enter Airport Abbreviation..'
+                            value={departingAirport}
+                            onChange={e => setDepartingAirport(e.target.value)}
+                        />
+                    </Form.Group>
+                    </Col>
+                    <Col>
+                    <Form.Group controlId="arrivingAirport">
+                        <Form.Label>Arriving Airport: </Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder='Enter Airport Abbreviation..'
+                            value={arrivingAirport}
+                            onChange={e => setArrivingAirport(e.target.value)}
+                        />
+                    </Form.Group>
+                    </Col>
+                </Row>
+                <Row className="mt-3">
+                    <Col>
+                    <Form.Group controlId="departureDate">
+                        <Form.Label>Departing Date: </Form.Label>
+                        <Form.Control
+                            type='date'
+                            placeholder='Enter Airport Abbreviation..'
+                            value={departingDate}
+                            onChange={e => setDepartingDate(e.target.value)}
+                        />
+                    </Form.Group>
+                    </Col>
+                    <Col>
+                    <Form.Group controlId="arrivingDate">
+                        <Form.Label>Return Date: </Form.Label>
+                        <Form.Control
+                            type='date'
+                            placeholder='Enter Airport Abbreviation..'
+                            value={arrivingDate}
+                            onChange={e => setArrivingDate(e.target.value)}
+                        />
+                    </Form.Group>
+                    </Col>
+                </Row>
+                <Row className="mt-4">
+                    <Col>
+                        <Button size="xs" onClick={createFlight}>Generate</Button>
+                    </Col>
+                </Row>
+            </Form>
+            </Container>
+        </div>
     );
 }
 
