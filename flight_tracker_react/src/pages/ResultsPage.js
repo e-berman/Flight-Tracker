0efe799c54fa894e1ffc40ab45b4cc75c35de172
e-berman <ethanjamesberman@gmail.com>
2022-03-01 23:50:29 -0800

    comment out microservice functionality until fix

--- flight_tracker_react/src/pages/ResultsPage.js
+++ flight_tracker_react/src/pages/ResultsPage.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
 import { Container, Row, Col, Button, Form, Navbar } from 'react-bootstrap';
 
 
